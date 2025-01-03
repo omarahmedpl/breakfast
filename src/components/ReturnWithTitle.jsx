ab14aaa912a74403eede0975862e077f2316f59f
omarahmedpl <97605387+omarahmedpl@users.noreply.github.com>
2025-01-03 18:07:24 +0200

    Update ReturnWithTitle.jsx

--- src/components/ReturnWithTitle.jsx
+++ src/components/ReturnWithTitle.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReturnButton from "./ReturnButton";
+import { HomeIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const ReturnWithTitle = ({ title, classCustom, ...props }) => {
+  const navigate = useNavigate();
   return (
     <div
       className={`title-icon w-100 flex justify-between items-center flex-row-reverse container ${classCustom}`}
     >
       <ReturnButton />
+      <div className="flex flex-row-reverse gap-2 justify-center items-center">
       <h1 className="text-3xl font-bold">{title}</h1>
+      <button onClick={() => navigate('/')}>
+        <HomeIcon
+          size={45}
+          className="cursor-pointer hover:scale-110 duration-200"
+        />
+      </button>
+      </div>
     </div>
   );
 };
